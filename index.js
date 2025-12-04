c9b074c3b2816c98e0aec08767cc9d0993d68b89
isaacprince <isaacprince002@gmail.com>
2025-12-04 16:15:21 -0500

    feat: Add Js for fucntionality

---
+++ index.js
@@ -0,0 +1,31 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+
+mySubmit.onclick = function(){
+   
+   if(myCheckBox.checked){
+      subResult.textContent = `You are subscribed!`;
+   }
+   else{
+      subResult.textContent = `You are not subscribed!`;
+   }
+
+   if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa card!`;
+   }
+   else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with Mastercard!`;
+   }
+   else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal!`;
+   }
+   else{
+        paymentResult.textContent = `Select a payment method.`;
+   }
+}
